7dcd4b976e9508c1cce083422d8587a649219990
aleksandrtihonkih916-crypto <aleksandrtihonkih916@gmail.com>
2025-11-12 22:08:31 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TELEGRAM_BOT_TOKEN = "8354002591:AAFnJENcfm-CgtchCFW-NdtXS5j8HkKo-z0"
+YANDEX_LAVKA_URL = "https://lavka.yandex.ru/api/v1"
+TARGET_ADDRESS = "среднерогатская 20 санкт-петербург"
+CHECK_INTERVAL = 60  # секунды
